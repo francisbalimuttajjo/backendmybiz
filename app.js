465468c3e77f2891fe0f66e499291958a28445ca
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-05-18 12:37:53 +0300

    configuring vercel

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send({ msh: "logged in" });
+});
+
 app.use("/api/v1", stockItemRouter);
 app.use("/api/v1", userRouter);
 app.use("/api/v1", salesRouter);
